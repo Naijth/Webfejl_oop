029b2391222b5a118ff85e1f4a777299db5b9c61
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2024-11-29 15:12:40 +0100

    added play and gettierlevel functions to player

--- basics/script.js
+++ basics/script.js
@@ -2,5 +2,19 @@ function Player(nickname){
     this.nickname = nickname;
     this.playedMatch = 0;
 }
+Player.prototype.play = function(){
+    this.playedMatch++;
+    console.log(this.nickname + this.playedMatch)
+}
+Player.prototype.getTierlevel = function(){
+    if (this.playedMatch < 4){
+        return "A";
+    } else if (this.playedMatch < 7){
+        return "B";
+    } else{
+        return "C";
+    }
+}
 const player0 = new Player("gomszab")
 console.log(player0)
+player0.play()
