66fb324a2677739cf8f16bfbb5f896902ace9e2a
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2024-12-13 14:42:25 +0100

    Added a whole lotta stuff I am laz to explain

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,39 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+/**
+ * With this fancy, shiny, fresh and brand new method you can now use a complex variable in yur constructor. Truly fascinating and makes me go "awooga".
+ */
+class Person {
+    constructor(obj){
+        this.firstname1 = obj.firstname1;
+        this.firstname2 = obj.firstname2;
+        this.lastname = obj.lastname;
+    }
+    /**
+     * This beautiful babygirl adds the values in the constructor to the called variable
+     */
+    render(parentElement){
+        const tr = document.createElement("tr");
+        parentElement.appendChild(tr);
+        const td1 = document.createElement("td");
+        td1.innerHTML = this.firstname1;
+        tr.appendChild(td1);
+        if(this.firstname2 == undefined){
+            td1.rowSpan = 2;
+        } else{
+            const td2 = document.createElement("td");
+            td2.innerHTML = this.firstname2;
+            tr.appendChild(td2);
+        }
+        const td3 = document.createElement("td");
+        td3.innerHTML = this.lastname;
+        tr.appendChild(td3)
+    }
+}
+function init(){
+    for (const person of array){
+        const per = new Person(person)
+        
+    }
+}
