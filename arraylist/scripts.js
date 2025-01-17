d442be26fe08dbac437b27b22198232d2e84cc13
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2025-01-17 15:31:35 +0100

    stuff

---
+++ arraylist/scripts.js
@@ -0,0 +1,60 @@
+class ArrayList {
+    /**
+     * stores the length of the array list
+     * @type {Number}
+     */
+    #length; 
+    #state;
+
+    get Count(){
+        return this.#length;
+    }
+
+    constructor(){
+        this.#length = 0;
+        this.#state = {};
+    }
+
+    Add(item) {
+        const index = this.#length; //creates the index whch stures the current length
+        this.#state[index] = item; //gives the state object the input value at the index variable's value
+        //with this we can reach the added item
+        Object.defineProperty(this, index, 
+            {
+                get: function(){
+                    return this.#state[index];
+                },
+                set: function(value){
+                    this.#state[index] = value;
+                },
+                writable: true
+            })
+        this.#length++; //increments length by one
+    }
+
+    Clear(){
+        this.#length = 0; //sets it back to og value
+        this.#state = {}; //sets it back to og value
+    }
+}
+
+/**
+ * Just an example
+ * const kakas = {};
+ * Object.defineProperty(kakas, 'nev', 
+ *     {
+ *         value: 'Géza',
+ *         writable: true
+ *     })
+ * kakas.nev = 'asd';
+ * console.log(kakas);
+ */
+
+/**
+ * Also an example
+ * const pers = {};
+ * pers.a = 'Ferenc';
+ * pers['a'] = 'Józsi';
+ * pers[0] = 'Tojás';
+ * console.log(pers);
+ */
