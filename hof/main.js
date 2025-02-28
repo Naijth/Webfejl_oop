973653dfe270b137c28c89c607bead187a4aa26f
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2025-02-28 08:43:27 +0100

    my brain hurt

---
+++ hof/main.js
@@ -0,0 +1,30 @@
+const fv1 = (a, b) => {
+    return a + b;
+}
+
+const fv2 = (a, b, cb) => {
+    const v1 = cb(a, 2);
+    const v2 = cb(b, 4);
+    return cb(v1, v2);
+}
+const res1 = fv2(5, 7, (a, b) => {
+    return a + b;
+})
+
+const fv3 = (op) => {
+    if (op == '-'){
+        return (a, b) => {
+            return a - b;
+        }
+    } else if (op == '*2'){
+        const multi = 2;
+        return (a, b) => {
+            return multi * (a + b);
+        }
+    }
+}
+const fv4 = fv3('-');
+
+console.log(res1);
+console.log(fv4(5, 7));
+console.log(fv2(5, 7, fv3('-')));
